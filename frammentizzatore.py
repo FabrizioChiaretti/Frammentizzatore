ce9a4fc8fd5a8941cabc063a3bc9da085a068dba
FabrizioChiaretti <chiaretti.1918799@studenti.uniroma1.it>
2024-05-24 20:08:44 +0200

    fragment header succefully inserted in the original packets

--- frammentizzatore.py
+++ frammentizzatore.py
@@ -1,58 +1,48 @@
+from scapy.all import IPv6, IPv6ExtHdrFragment, raw
+from random import getrandbits
 
-
-from netfilterqueue import NetfilterQueue
-import platform
-from FirewallHandler import FirewallHandler
-
-
-def frammentizzatore(packet):
-
-    print("traffic intercepted")
-
-    print("processing traffic...")
-    packet1 = packet
-    packet1.accept()
-
-    print("frammentizzatore ends")
-    return
-
-
-def setFirewallRules(protocol = "", src_ipv6addr= "::", dest_ipv6addr="::"):
+class frammentizzatore:
     
-    os_type = platform.system().lower()
-    print("os_type:", os_type)
-
-    print("setting firewall rules...")
-    firewall_handler = FirewallHandler(os_type, protocol, src_ipv6addr, dest_ipv6addr)
-    firewall_handler.insert_firewall_rules()
-    print("firewall rules set")
-
-    return firewall_handler
-
-
-
-def main():
-
-    firewall_handler = setFirewallRules()
-
-    nfqueue = NetfilterQueue()
-    print("nfqueue created")
-
-    nfqueue.bind(1, frammentizzatore)
-    print("bind to queue 1 executed")
-
-    try:
-        nfqueue.run()
-    except KeyboardInterrupt:
-        print("stop frammentizzatore")
-    finally:
-        firewall_handler.delete_firewall_rules()
-        nfqueue.unbind()
+    def __init__(self, max_fragment_lenght = 1280):
+        self.max_fragment_leght = max_fragment_lenght
     
-    print("exiting...")
-
-
-
-if __name__ == "__main__":
-    main()
-
+    
+    def fragment(self, input_packet, num_of_fragments):
+        
+        packet = IPv6(input_packet.get_payload())
+        
+        if IPv6ExtHdrFragment in packet:
+            print("IPv6ExtHdrFragment already present")
+            return packet
+        if IPv6 not in packet:
+            print("Can not found IP header")
+            return packet
+        
+        basic_header = packet.copy()
+        basic_header.remove_payload()
+        
+        first_fragment = basic_header.copy()
+        headers_to_skip = [0,60,43] # Hop-by-Hop Options, Destination Options, Routing Header
+        i = 0
+        
+        while (int(packet[i].nh) in headers_to_skip):
+            first_fragment = first_fragment / packet[i+1]
+            i +=1
+            continue
+        
+        first_fragment[i].nh = 44
+        random_value = getrandbits(32)
+        first_fragment = first_fragment / IPv6ExtHdrFragment(nh = packet[i].nh, m=1, id = random_value)
+        #first_fragment.show()
+        
+        input_payload = packet[i].payload.copy() # fragmentable part
+        #input_payload.show()
+        
+        first_fragment = first_fragment / input_payload
+        first_fragment.plen = len(raw(first_fragment.payload))
+        #first_fragment.show()
+        
+        
+        
+        
+        return first_fragment
---
+++ runner.py
@@ -0,0 +1,61 @@
+
+
+from netfilterqueue import NetfilterQueue
+import platform
+from FirewallHandler import FirewallHandler
+from frammentizzatore import frammentizzatore
+
+def traffic_handler(packet):
+
+    print("traffic intercepted")
+    print("processing traffic...")
+    number_of_fragments = 3
+    framm = frammentizzatore()
+    pkt = framm.fragment(packet, number_of_fragments)
+    
+    packet.set_payload(bytes(pkt))
+    packet.accept()
+    
+    print("frammentizzatore ends")
+    return
+
+
+def setFirewallRules(protocol = "", src_ipv6addr= "::", dest_ipv6addr="::"):
+    
+    os_type = platform.system().lower()
+    print("os_type:", os_type)
+
+    print("setting firewall rules...")
+    firewall_handler = FirewallHandler(os_type, protocol, src_ipv6addr, dest_ipv6addr)
+    firewall_handler.insert_firewall_rules()
+    print("firewall rules set")
+
+    return firewall_handler
+
+
+
+def main():
+
+    firewall_handler = setFirewallRules()
+
+    nfqueue = NetfilterQueue()
+    print("nfqueue created")
+
+    nfqueue.bind(1, traffic_handler)
+    print("bind to queue 1 executed")
+
+    try:
+        nfqueue.run()
+    except KeyboardInterrupt:
+        print("stop frammentizzatore")
+    finally:
+        firewall_handler.delete_firewall_rules()
+        nfqueue.unbind()
+    
+    print("exiting...")
+
+
+
+if __name__ == "__main__":
+    main()
+
